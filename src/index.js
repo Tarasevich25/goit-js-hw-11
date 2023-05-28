b810253c6d26a1df39730c390fe3b732eb9b1ef6
Taras Oliynik <112812514+Tarasevich25@users.noreply.github.com>
2023-05-28 11:57:21 +0300

    updated

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import './css/main.css';
+import { searchForm, gallery, btnAddLoad } from './js/const';
+import { fetch } from './js/fetchFunc';
+import {
+  alertFound,
+  alertEmptySearch,
+  alertNoSuchImages,
+  alertEndSearch,
+} from './js/alert';
+import { createCard } from './js/createCard';
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let q = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
+
+searchForm.addEventListener('submit', onSearchForm);
+btnAddLoad.addEventListener('click', onLoadMore);
+
+function onSearchForm(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0 });
+  page = 1;
+  q = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  btnAddLoad.classList.add('hidden');
+
+  if (q === '') {
+    alertEmptySearch();
+    return;
+  }
+
+  fetch(q, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        alertNoSuchImages();
+      } else {
+        createCard(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery').refresh();
+        alertFound(data);
+
+        if (data.totalHits > perPage) {
+          btnAddLoad.classList.remove('hidden');
+        }
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchForm.reset();
+    });
+}
+
+function onLoadMore() {
+  page += 1;
+  simpleLightBox.destroy();
+
+  fetch(q, page, perPage)
+    .then(({ data }) => {
+      createCard(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+      const pagesTotal = Math.ceil(data.totalHits / perPage);
+
+      if (page > pagesTotal) {
+        btnAddLoad.classList.add('hidden');
+        alertEndSearch();
+      }
+    })
+    .catch(error => console.log(error));
+}
---
+++ src/js/const.js
@@ -0,0 +1,5 @@
+export const BASE_URL = 'https://pixabay.com/api/';
+export const API_KEY = '36825505-44f4018b00ca78f01b38a4fb9';
+export const searchForm = document.querySelector('#search-form');
+export const gallery = document.querySelector('.gallery');
+export const btnAddLoad = document.querySelector('.btn-add-load');
